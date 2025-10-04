ddac988b87526e82b5544947999c9f1b329a9185
SnackOverflow707 <131423796+SnackOverflow707@users.noreply.github.com>
2025-10-04 10:18:40 -0700

    example images and app framework

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+
 import './App.css';
+import happyImg from "tamagachi guys/happy.png";
+import sadImg from "tamagachi guys/sad.png";
+import lpaImg from "tamagachi guys/lpa.png";
+import reImg from "tamagachi guys/re.png";
+import maskImg from "tamagachi guys/mask.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useState } from "react";
+
+export default function AirTamagotchi() {
+  //Variable declarations: will need to be API filled
+  const maskNeeded = airQualityIndex > 100; 
+  const lowExertion = airQualityIndex > 50;
+  const reduceExposure = airQualityIndex > 75;
+  const climateAreas = airQualityIndex > 150;
+
+  var airQualityIndex = 75; // Filler value (will be replaced with API data)
+  var happinessLevel = 50;
+
+  //Get picture based on mood
+  const getImage = () => {
+    switch (mood) {
+      case "happy":
+        return happyImg;
+      case "sad":
+        return sadImg;
+      case "mask":
+        return maskImg;
+      case "lpa":
+        return lpaImg;
+      case "re":
+          return reImg;
+      default:
+        return happyImg;
+    }
+  };
+
+  
+
 }
-
-export default App;
