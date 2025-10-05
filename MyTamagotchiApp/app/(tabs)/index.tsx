b4304210061f2da642a19c0c3211a59a587a50a1
Annabel <li.annabel47@gmail.com>
2025-10-04 21:41:41 -0700

    adding index.tsx as well

--- MyTamagotchiApp/app/(tabs)/index.tsx
+++ MyTamagotchiApp/app/(tabs)/index.tsx
@@ -56,7 +56,7 @@ export default function TabTwoScreen() {
       console.log("Sending request to backend...");
 
       //Changed port from 8081 to 5000
-      const response = await fetch('http://localhost:5001/aqi', {
+      const response = await fetch('http://localhost:5001/get_data', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
